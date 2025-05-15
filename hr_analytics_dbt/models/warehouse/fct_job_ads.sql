cf2b1682b06bf4e0b572862c4f850f4496017670
Joakim Olausson <joakimdolausson@hotmail.com>
2025-05-15 18:40:19 +0200

    Skapat och uppdaterat fct_job_ads enligt datamodellen i uppgiften

--- hr_analytics_dbt/models/warehouse/fct_job_ads.sql
+++ hr_analytics_dbt/models/warehouse/fct_job_ads.sql
@@ -1,11 +1,16 @@
 -- models/warehouse/fct_job_ads.sql
 
 SELECT
-    job_id,
-    occupation_id,
-    occupation_name,
-    relevance,
-    application_deadline,
-    employer_name,
-    workplace_city
+  job_id,
+  occupation_id,
+  {{ dbt_utils.generate_surrogate_key([
+    'headline',
+    'description_text_formatted',
+    'employment_type'
+  ]) }} AS job_details_id,
+  employer_id,
+  NULL AS auxiliary_attributes_id, -- Placeholder
+  relevance,
+  application_deadline
 FROM {{ ref('stg_job_ads') }}
+WHERE job_id IS NOT NULL
