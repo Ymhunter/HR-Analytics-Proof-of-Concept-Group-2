19c2d2bff21c0b5e6dabe798712000dc0df84f0a
Tobias Lampinen <tobias.lampinen@student.nbi-handelsakademin.se>
2025-05-26 18:31:17 +0200

    Fullföljde datavalidering för dbt staging, koppling till job_ads_dataset.job_ads bekräftad. Skapade hjälpskript för att inspektera tabeller och kolumner, validerade datakällor och tabellstruktur. Rensade upp tillfälliga .py-filer efter att staging-modellen stg_job_ads körts utan fel.

---
+++ models/example/staging/stg_job_ads.sql/stg_job_ads.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='view') }}
+
+SELECT
+    id,
+    headline,
+    employer__name AS employer_name,
+    workplace_address__region AS region,
+    occupation_group__label AS occupation_group,
+    description__text AS description_text,
+    working_hours_type__label AS working_hours,
+    duration__label AS duration_label,
+    publication_date,
+    application_deadline,
+    occupation__label AS occupation_label,
+    removed
+FROM job_ads_dataset.job_ads
