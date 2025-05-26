5f8266e7937d11b4c9d57d4d720f752858f3cef7
Tobias Lampinen <tobias.lampinen@student.nbi-handelsakademin.se>
2025-05-26 20:40:03 +0200

    la upp fel mart för it när det ska vara försäljning, inköp,marknadsföring justerat och kör med lyckat resultat

---
+++ models/example/Mart/mart/sales_mart.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='view') }}
+
+SELECT
+  occupation_label AS occupation,
+  COUNT(*) AS number_of_ads,
+  duration_label,
+  MIN(publication_date) AS earliest_publication,
+  MAX(application_deadline) AS latest_deadline
+FROM {{ ref('stg_job_ads') }}
+WHERE occupation_group = 'Försäljning, inköp, marknadsföring'
+GROUP BY occupation_label, duration_label
+ORDER BY number_of_ads DESC
---
+++ target/run/hr_project/models/example/Mart/mart/sales_mart.sql
@@ -0,0 +1,16 @@
+
+  
+  create view "job_ads_pipeline"."main"."sales_mart__dbt_tmp" as (
+    
+
+SELECT
+  occupation_label AS occupation,
+  COUNT(*) AS number_of_ads,
+  duration_label,
+  MIN(publication_date) AS earliest_publication,
+  MAX(application_deadline) AS latest_deadline
+FROM "job_ads_pipeline"."main"."stg_job_ads"
+WHERE occupation_group = 'Försäljning, inköp, marknadsföring'
+GROUP BY occupation_label, duration_label
+ORDER BY number_of_ads DESC
+  );
